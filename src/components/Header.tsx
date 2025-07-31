d83e41aced01b35012c7a87d3c91f03036747667
Raphael <raphaelgarnier1997@gmail.com>
2025-07-31 20:02:53 +0200

    styling header components with tailwind CSS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,31 @@
 import Link from "next/link";
 import Logo from "./Logo";
 
+const routes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "All Events",
+    path: "/events/all",
+  },
+];
+
 export default function Header() {
   return (
-    <header>
+    <header className="flex justify-between items-center border-b border-white/10 px-3 h-14 sm:px-9">
       <Logo />
-      <Link href="/">Home</Link>
-      <Link href="/events/all">All Events</Link>
+
+      <nav>
+        <ul className="flex gap-x-6 text-sm">
+          {routes.map(route => (
+            <li key={route.path} className="text-white/50 hover:text-white transition">
+              <Link href={route.path}>{route.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }

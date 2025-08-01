107ea34e4fa833fc8bf23b4f5d2920179cab717e
Raphael <raphaelgarnier1997@gmail.com>
2025-08-01 02:57:50 +0200

    fetching event data with dynamic city name

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -6,17 +6,21 @@ type EventPageProps = {
   };
 };
 
-export default function CityEventsPage({ params }: EventPageProps) {
-  const FirstLetterUpperCase = (city: string) => {
-    const cityName = city.slice(0, 1).toUpperCase() + params.city.slice(1);
-    return cityName;
-  };
+export default async function CityEventsPage({ params }: EventPageProps) {
+  const city = params.city;
+
+  const response = await fetch(
+    `https://bytegrad.com/course-assests/projects/evento/api/events?city=${city}`
+  );
+  const events = response.json();
+  console.log(events);
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <Title>
-        {params.city === "all" && "All Events"}
-        {params.city !== "all" && `Events in ${FirstLetterUpperCase(params.city)}`}
+        {city === "all"
+          ? "All Events"
+          : `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </Title>
     </main>
   );
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -14,8 +14,9 @@ export default function SearchForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="w-full sm:w-[580px]" action="">
+    <form id="searchForm" onSubmit={handleSubmit} className="w-full sm:w-[580px]" action="">
       <input
+        id="searchForm__input"
         value={searchText}
         onChange={e => setSearchText(e.target.value)}
         className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10"

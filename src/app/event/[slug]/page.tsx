87041d3fddd0c765ad46ad10c45e171dfa24e8a7
Raphael <raphaelgarnier1997@gmail.com>
2025-08-01 15:42:30 +0200

    fetching data in the EventPage component

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,13 @@
-export default function EventPage() {
+type EventPageProps = {
+  params: Promise<{ slug: string }>;
+};
+
+export default async function EventPage({ params }: EventPageProps) {
+  const { slug } = await params;
+
+  const res = await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`);
+  const data = await res.json();
+  console.log(data);
+
   return <main>Singular Event</main>;
 }

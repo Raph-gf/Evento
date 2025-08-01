598dffcb5d4e80fb807b875177b90b91a6bbff18
Raphael <raphaelgarnier1997@gmail.com>
2025-08-01 19:47:02 +0200

    creating a capitalize function in the utils.ts

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -2,10 +2,19 @@ import EventsList from "@/components/events-list";
 import Title from "@/components/title";
 import { Suspense } from "react";
 import LoadingCityEvents from "./loading";
+import { capitalize } from "@/lib/utils";
 
 type EventPageProps = {
   params: Promise<{ city: string }>;
 };
+
+export async function generateMetadata({ params }: EventPageProps) {
+  const { city } = await params;
+  return {
+    title: `Event in ${city}`,
+    description: "Browse more than 10,000 events worldwide",
+  };
+}
 
 export default async function CityEventsPage({ params }: EventPageProps) {
   const { city } = await params;
@@ -13,9 +22,7 @@ export default async function CityEventsPage({ params }: EventPageProps) {
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh] ">
       <Title className="mb-28">
-        {city === "all"
-          ? "All Events"
-          : `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+        {city === "all" ? "All Events" : `Events in ${capitalize(city)}`}
       </Title>
 
       <Suspense fallback={<LoadingCityEvents />}>

4b50b44a693dac99b3921f8f8fbc6516ea2a3cc1
Raphael <raphaelgarnier1997@gmail.com>
2025-08-01 16:21:26 +0200

    More Styling ...

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import Title from "@/components/title";
 import Image from "next/image";
 
 type EventPageProps = {
@@ -7,22 +8,57 @@ type EventPageProps = {
 export default async function EventPage({ params }: EventPageProps) {
   const { slug } = await params;
 
-  const res = await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`);
+  const res = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
   const event = await res.json();
-  console.log(event);
 
   return (
     <main>
-      <section className="relative h-[361px] overflow-hidden">
+      <section className="relative overflow-hidden flex justify-center items-center py-14 md:py-20">
         <Image
-          className="object-cover blur-3xl "
+          className="object-cover z-0 blur-3xl "
           src={event.imageUrl}
-          alt="Event-Image-background"
+          alt="Event Image background"
           fill
-          sizes="(max-width:1280px) 100wv,1280px"
-        ></Image>
+          quality={50}
+          sizes="(max-width:1280px) 100wv, 1280px"
+          priority
+        />
+
+        <div className="z-1 relative flex flex-col gap-6 lg:gap-16 lg:flex-row ">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+            className="rounded-xl border-2 border-white/50 object-cover"
+          />
+
+          <div className="flex flex-col">
+            <p className="text-white/75">
+              {new Date(event.date).toLocaleDateString("en-US", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+
+            <Title className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {event.name}
+            </Title>
+
+            <p className="whitespace-nowrap text-xl text-white/75">
+              {" "}
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+
+            <button className="bg-white/20 text-lg capitalize mt-5 lg:mt-auto w-[95px] sm:w-full py-2 rounded-md border-white/10 border-2 focus:scale-105 hover:scale-105 active:scale-[1.02] transition">
+              Get ticket
+            </button>
+          </div>
+        </div>
       </section>
-      <div></div>
     </main>
   );
 }

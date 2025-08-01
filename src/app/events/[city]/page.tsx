ae01431f2b09ffcfcb0d9c8c6dccc54f055273c5
Raphael <raphaelgarnier1997@gmail.com>
2025-08-01 02:39:59 +0200

    Adding dynamic Title to the CityEventsPage

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,9 +1,23 @@
 import Title from "@/components/Title";
 
-export default function CityEventsPage() {
+type EventPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+export default function CityEventsPage({ params }: EventPageProps) {
+  const FirstLetterUpperCase = (city: string) => {
+    const cityName = city.slice(0, 1).toUpperCase() + params.city.slice(1);
+    return cityName;
+  };
+
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
-      <Title>Events in Austin</Title>
+      <Title>
+        {params.city === "all" && "All Events"}
+        {params.city !== "all" && `Events in ${FirstLetterUpperCase(params.city)}`}
+      </Title>
     </main>
   );
 }

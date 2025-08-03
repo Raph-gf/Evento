85094e5cd3293ad42fe604d64c5c4df114879db2
Raphael <raphaelgarnier1997@gmail.com>
2025-08-03 19:42:19 +0200

    creating db.ts file and refactoring all fetch call to prisma

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { EventoEvent } from "@/generated/prisma";
+import prisma from "./db";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -11,27 +11,19 @@ export function capitalize(str: string) {
 }
 
 export async function getEvents(city: string) {
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`,
-    {
-      next: {
-        revalidate: 300,
-      },
-    }
-  );
-  const events: EventoEvent[] = await response.json();
+  const events = await prisma.eventoEvent.findMany({
+    where: {
+      city: city === "all" ? undefined : capitalize(city),
+    },
+  });
   return events;
 }
 
 export async function getEvent(slug: string) {
-  const res = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`,
-    {
-      next: {
-        revalidate: 300,
-      },
-    }
-  );
-  const event: EventoEvent = await res.json();
+  const event = await prisma.eventoEvent.findUnique({
+    where: {
+      slug: slug,
+    },
+  });
   return event;
 }

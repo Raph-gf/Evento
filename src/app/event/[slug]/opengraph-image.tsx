6a8e55726cbcadbaea5095b36404283372bad028
Raphael <raphaelgarnier1997@gmail.com>
2025-08-05 13:49:17 +0200

    adding dynamic image in opengraph-image.tsx

--- src/app/event/[slug]/opengraph-image.tsx
+++ src/app/event/[slug]/opengraph-image.tsx
@@ -1,3 +1,4 @@
+import { getEvent } from "@/lib/server-utils";
 import { ImageResponse } from "next/og";
 
 export const alt = "Evento";
@@ -8,19 +9,26 @@ export const size = {
 export const contentType = "image/png";
 
 export default async function Image({ params }: { params: { slug: string } }) {
+  const event = await getEvent(params.slug);
+  console.log(event);
   return new ImageResponse(
     (
-      <div
-        style={{
-          fontSize: 48,
-          background: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
+      <div>
+        <img
+          src={event.imageUrl}
+          alt="Event background"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            filter: "blur(20px)",
+            opacity: 0.6,
+            zIndex: 0,
+          }}
+        />
         <h1>{params.slug}</h1>
         <p>Evento - Browse events around you</p>
       </div>
